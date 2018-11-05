f50162e4babd515d50b338fd1cb034088dc0c4b3
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-11-05 14:13:31 -0500

    Components and Grid for page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Header.js';
+import Main from './Main.js';
+import Sidebar from './Sidebar.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Header/>
+        <Main/>
+        <Sidebar/>
       </div>
     );
   }
