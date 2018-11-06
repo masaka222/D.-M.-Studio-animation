e57ba24ed538e3264824e8ce4233106aa3d72c3a
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-11-06 13:28:34 -0500

    Sidebar text-align and font implementation

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,8 +4,10 @@ import './Sidebar.css';
 class Sidebar extends Component {
   render() {
     return (
-      <div className="Sidebar">
-        Sidebar
+      <div className="sidebar">
+      <p className="sidebar-text">
+        Contact Us for Your Own Marketing Strategy
+      </p>
       </div>
     );
   }
