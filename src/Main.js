62e3c42f96a33f0e3aaada0b8dd672775a70d697
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-11-11 16:05:20 -0500

    Main Text Boxes + Hover animation

--- src/Main.js
+++ src/Main.js
@@ -5,25 +5,40 @@ class Main extends Component {
   render() {
     return (
       <div className="main">
-        <p className="main-we"> <span> W </span><span> E </span> </p>
-        <p className="main-line"></p>
-        <p className="main-are"> <span>A</span><span>R</span><span>E</span></p>
-        <p className="main-about" id="about-text"> 
-        <span>A</span><span>B</span><span>O</span><span>U</span><span>T</span>
-        </p>
-        <p className="main-experiences" id="experiences-text">
-        <span>E</span>
-        <span>X</span>
-        <span>P</span>
-        <span>E</span>
-        <span>R</span>
-        <span>I</span>
-        <span>E</span>
-        <span>N</span>
-        <span>C</span>
-        <span>E</span>
-        <span>S</span>
-        </p>
+        <div className="main-we"> 
+            <span> W </span>
+            <span> E </span> 
+        </div>
+
+        <div className="main-line"></div>
+
+        <div className="main-are"> 
+            <span>A</span>
+            <span>R</span>
+            <span>E</span>
+        </div>
+
+        <div className="main-about" id="about-text"> 
+            <span>A</span>
+            <span>B</span>
+            <span>O</span>
+            <span>U</span>
+            <span>T</span>
+        </div>
+        
+        <div className="main-experiences" id="experiences-text">
+            <span>E</span>
+            <span>X</span>
+            <span>P</span>
+            <span>E</span>
+            <span>R</span>
+            <span>I</span>
+            <span>E</span>
+            <span>N</span>
+            <span>C</span>
+            <span>E</span>
+            <span>S</span>
+        </div>
       </div>
     );
   }
