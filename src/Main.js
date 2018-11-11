8e81dadcbc520b9f80652390e2ea164521c153b7
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-11-10 23:03:43 -0500

    Main Text

--- src/Main.js
+++ src/Main.js
@@ -5,11 +5,25 @@ class Main extends Component {
   render() {
     return (
       <div className="main">
-        <p>WE</p>
-        <p>-</p>
-        <p>ARE</p>
-        <p id="about-text">ABOUT</p>
-        <p id="experiences-text">EXPERIENCES</p>
+        <p className="main-we"> <span> W </span><span> E </span> </p>
+        <p className="main-line"></p>
+        <p className="main-are"> <span>A</span><span>R</span><span>E</span></p>
+        <p className="main-about" id="about-text"> 
+        <span>A</span><span>B</span><span>O</span><span>U</span><span>T</span>
+        </p>
+        <p className="main-experiences" id="experiences-text">
+        <span>E</span>
+        <span>X</span>
+        <span>P</span>
+        <span>E</span>
+        <span>R</span>
+        <span>I</span>
+        <span>E</span>
+        <span>N</span>
+        <span>C</span>
+        <span>E</span>
+        <span>S</span>
+        </p>
       </div>
     );
   }
