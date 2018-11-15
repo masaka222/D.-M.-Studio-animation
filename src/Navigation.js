9abf1fd355bf94d477880388850956ce4ffb9c37
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-11-15 09:48:36 -0500

    Navigation text-box dissapearance final

--- src/Navigation.js
+++ src/Navigation.js
@@ -7,14 +7,30 @@ const Navigation = (props) => {
         if (document.getElementById("navi-toggle").checked == true) {
             /* White colors to the first line of the Grid */
             document.getElementById('we-text').style.color = "white";
+            document.getElementById('we-text').classList.add('hidden');
             document.getElementById('line-text').style.borderTop = "0.5rem solid white";
             document.getElementById('are-text').style.color = "white";
+            document.getElementById('are-text').classList.add('hidden');
+
+            /* Text and box dissapearance in the other lines */
+            document.getElementById('about-text').style.opacity = "0";
+            document.getElementById('about-text').classList.add('hidden');
+            document.getElementById('experiences-text').style.opacity = "0";
+            document.getElementById('experiences-text').classList.add('hidden');
         }
         else{
             /* Black colors to the first line of the Grid */
             document.getElementById('we-text').style.color = "black";
+            document.getElementById('we-text').classList.remove('hidden');
             document.getElementById('line-text').style.borderTop = "0.5rem solid black";
             document.getElementById('are-text').style.color = "black";
+            document.getElementById('are-text').classList.remove('hidden');
+
+            /* Text and box dissapearance in the other lines */
+            document.getElementById('about-text').style.opacity = "1";
+            document.getElementById('about-text').classList.remove('hidden');
+            document.getElementById('experiences-text').style.opacity = "1";
+            document.getElementById('experiences-text').classList.remove('hidden');
         }
     }
 
