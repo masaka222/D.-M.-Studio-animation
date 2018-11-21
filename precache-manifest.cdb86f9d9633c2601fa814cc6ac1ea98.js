b1b0942d1b58428bae35be9ba959395883e549d6
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-11-20 21:51:53 -0500

    Updates

---
+++ precache-manifest.cdb86f9d9633c2601fa814cc6ac1ea98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "142228c30a5d8a7734bdc10bcd346f59",
+    "url": "/D.-M.-Studio-animation/static/media/Komoda.142228c3.otf"
+  },
+  {
+    "revision": "f6e15d2bbc48f99802ebfcfe09543a95",
+    "url": "/D.-M.-Studio-animation/static/media/DM-background.f6e15d2b.jpg"
+  },
+  {
+    "revision": "2d91301ec3674240c06a",
+    "url": "/D.-M.-Studio-animation/static/js/runtime~main.2d91301e.js"
+  },
+  {
+    "revision": "420239a5d3ec1f9db4c6",
+    "url": "/D.-M.-Studio-animation/static/js/main.420239a5.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/D.-M.-Studio-animation/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "420239a5d3ec1f9db4c6",
+    "url": "/D.-M.-Studio-animation/static/css/main.30de88b2.chunk.css"
+  },
+  {
+    "revision": "0942870d134576cf6b596e3d5eb8ed14",
+    "url": "/D.-M.-Studio-animation/index.html"
+  }
+];
